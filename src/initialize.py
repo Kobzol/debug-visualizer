874a3c58316c0db036a15812f00c377849608400
Jakub Beránek <berykubik@gmail.com>
2015-04-20 10:16:56 +0200

    added localization script

--- src/initialize.py
+++ src/initialize.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
 import gettext
 import locale
 
+lang_path = "res/lang"
+language_file = os.path.join(lang_path, "en_GB.mo")
+
 def init_localization():
-  locale.setlocale(locale.LC_ALL, '') # use user's preferred locale
-  loc = locale.getlocale()
-  filename = "res/lang/messages_%s.mo" % locale.getlocale()[0][0:2]
+  #locale.setlocale(locale.LC_ALL, '') # use user's preferred locale
+  #loc = locale.getlocale()
+  filename = language_file
  
   try:
     trans = gettext.GNUTranslations(open( filename, "rb" ) )
