1ab4c5ff438badb0bfcc4ede43d057b85daced92
Jakub Beránek <berykubik@gmail.com>
2015-05-31 23:27:20 +0200

    Added debugger server and command

---
+++ debugger/debugcommand.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from enum import IntEnum
+import json
+from sys import byteorder
+
+class CommandType(IntEnum):
+    LoopbackCommand = 1
+
+class DebugCommand(object):
+    @staticmethod
+    def receive(client):
+        length = int.from_bytes(client.recv(4), byteorder = "little")
+                
+        if length < 1:
+            raise IOError()
+                
+        data = json.loads(client.recv(length).decode(encoding="utf_8"))
+        return DebugCommand(data["type"], data)
+    
+    def __init__(self, type, data):
+        self.type = type
+        self.data = data
+        self.data["type"] = type
+    
+    def send(self, socket):
+        serialized_data = self.get_serialized_data()
+        length = len(serialized_data)
+        socket.sendall(length.to_bytes(4, byteorder = "little"))
+        socket.sendall(serialized_data)
+        
+    def get_serialized_data(self):
+        return json.dumps(self.data).encode(encoding="utf_8")
---
+++ debugger/debugserver.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import socket
+import threading
+import select
+import json
+import sys
+
+from debugcommand import CommandType, DebugCommand
+
+class DebugServer(object):
+    def __init__(self, port):
+        self.address = ("localhost", port)
+        self.running = False
+        self.connected_client = None
+        
+    def is_running(self):
+        return self.running
+
+    def is_client_connected(self):
+        return self.connected_client is not None
+
+    def start(self):
+        if not self.is_running():
+            self.create_socket()
+            self.server_thread = threading.Thread(target=self.run_server_thread)
+            self.running = True
+            self.server_thread.start()
+    
+    def stop(self):
+        if self.is_running():
+            self.running = False
+            self.server_thread.join()
+            self.server.close()
+    
+    def create_socket(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.address)
+        self.server.listen(0)
+    
+    def is_data_available(self, fd, timeout = 0.2):
+        return len(select.select([fd], [], [], timeout)[0]) != 0
+    
+    def run_server_thread(self):
+        while self.is_running():
+            try:
+                if self.is_data_available(self.server, 0.2) and not self.is_client_connected():
+                    client, address = self.server.accept()
+                    self.handle_client(client, address)
+            except:
+                pass
+    
+    def handle_command(self, command):
+        if command.type == CommandType.LoopbackCommand:
+            command.send(self.connected_client)
+    
+    def handle_client(self, client, address):
+        self.connected_client = client
+        
+        try:
+            while self.is_running():
+                if self.is_data_available(client, 0.2):
+                    self.handle_command(DebugCommand.receive(client))
+        except:
+            pass
+        finally:
+            self.client_connected = None
