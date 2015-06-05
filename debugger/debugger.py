d4c4d89d13b4bd275c5076d211edf05312611ce8
Jakub Beránek <berykubik@gmail.com>
2015-06-05 23:00:02 +0200

    ENH: debugger states

--- debugger/debugger.py
+++ debugger/debugger.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import gdb
-import memory_bp
+import traceback
+import enum
+
 from gdb_helper import GdbHelper
 from memory import Memory
+from thread_manager import ThreadManager
+
+class DebuggerState(enum.IntEnum):
+    Stopped = 1
+    Running = 2
+    Exited = 3
 
 class Debugger(object):
     def __init__(self, options):
@@ -11,42 +19,70 @@ class Debugger(object):
         self.add_breakpoint("malloc", self.handle_malloc)
         self.add_breakpoint("free", self.handle_free)
         
-        self.exit_flag = False
+        self.state = DebuggerState.Exited
+        self.manual_stepping = False
+        
         self.memory = Memory()
         self.gdb_helper = GdbHelper()
+        self.thread_manager = ThreadManager()
         
         gdb.events.stop.connect(self.handle_break)
         gdb.events.exited.connect(self.handle_exit)
         
         if "valgrind_pid" in options:
             gdb.execute("target remote | vgdb --pid=" + str(options["valgrind_pid"]))
+            self.state = DebuggerState.Running
+            
+        self.add_breakpoint("main")
+        self.command_run()
     
     def handle_exit(self, exit_event):
-        self.exit_flag = True
+        self.state = DebuggerState.Exited
     
     def handle_break(self, stop_event):
-        if isinstance(stop_event, gdb.SignalEvent):
-            print("ERROR: " + str(stop_event.stop_signal))
-        
-        if not isinstance(stop_event, gdb.BreakpointEvent) or self.exit_flag:
+        if self.state == DebuggerState.Exited:
             return
         
-        for bp, callback in self.breakpoints:
-            if bp in stop_event.breakpoints:
-                if not callback():
-                    self.command_continue()
-                break
+        self.state = DebuggerState.Stopped
+        
+        try:
+            locals = self.thread_manager.get_thread_vars()[0]
+            self.memory.check_pointers(locals)
+            
+            if isinstance(stop_event, gdb.SignalEvent):
+                print("ERROR: " + str(stop_event.stop_signal))
+            
+            if not isinstance(stop_event, gdb.BreakpointEvent):
+                return
+            
+            for bp, callback in self.breakpoints:
+                if bp in stop_event.breakpoints:
+                    if callback and not callback() and not self.manual_stepping:
+                        self.command_continue()
+                    break
+        except:
+            print(traceback.format_exc())
     
     def command_continue(self):
+        self.state = DebuggerState.Running
+        self.manual_stepping = False
         gdb.execute("continue")
     
     def command_finish(self):
+        self.state = DebuggerState.Running
         gdb.execute("finish")
     
     def command_run(self):
+        self.state = DebuggerState.Running
+        self.manual_stepping = False
         gdb.execute("run")
     
-    def add_breakpoint(self, location, callback):
+    def command_next(self):
+        self.state = DebuggerState.Running
+        self.manual_stepping = True
+        gdb.execute("next")
+    
+    def add_breakpoint(self, location, callback=None):
         self.breakpoints.append((gdb.Breakpoint(location, internal=True), callback))
         
     def handle_malloc(self):
@@ -60,5 +96,6 @@ class Debugger(object):
 
     def handle_free(self):
         address = self.gdb_helper.get_args()[0].value
-        
         self.memory.free(address)
+        
+        return False
