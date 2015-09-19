3ed2c5c6e2c8573092e59ae251cff079800a0553
Jakub Beránek <berykubik@gmail.com>
2015-09-19 11:55:51 +0200

    ENH: added check for exception when getting file and line location

--- debugger/file_manager.py
+++ debugger/file_manager.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import gdb
+import sys
 
 class FileManager(object):
     """
     Returns tuple (filename, line_number).
     """
     def get_current_location(self):
-        frame = gdb.selected_frame()
-        sal = frame.find_sal()
-        symtab = sal.symtab
-        
-        return (symtab.fullname(), sal.line)
+        try:     
+            frame = gdb.selected_frame()
+            sal = frame.find_sal()
+            symtab = sal.symtab
+            
+            return (symtab.fullname(), sal.line)
+        except:
+            return None
