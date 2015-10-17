65831a6ce6459a90edf486c52068af082b3a6f37
Jakub Beránek <berykubik@gmail.com>
2015-10-17 17:57:01 +0200

    REF: moved RectangleBBox and Margin to geometry.py

--- gui/drawing/drawable.py
+++ gui/drawing/drawable.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import abc
+
+from drawing.geometry import Margin, RectangleBBox
 from drawing.size import Size
 from drawing.vector import Vector
+from mouse import ClickHandler
 
 
 class Color(object):
@@ -140,59 +143,11 @@ class DrawingUtils(object):
         cr.restore()
 
 
-class RectangleBBox(object):
-    @staticmethod
-    def contain(bboxes):
-        min_x = bboxes[0].x
-        max_x = bboxes[0].x + bboxes[0].width
-        min_y = bboxes[0].y
-        max_y = bboxes[0].y + bboxes[0].height
-
-        for bbox in bboxes[1:]:
-            min_x = min(min_x, bbox.x)
-            max_x = max(max_x, bbox.x + bbox.width)
-            min_y = min(min_y, bbox.y)
-            max_y = max(max_y, bbox.y + bbox.height)
-
-        return RectangleBBox((min_x, min_y), (max_x - min_x, max_y - min_y))
-
-    def __init__(self, position=(0, 0), size=(0, 0)):
-        self.position = Vector.vectorize(position)
-        self.size = Size.make_size(size)
-
-        self.x = self.position.x
-        self.y = self.position.y
-        self.width = self.size.width
-        self.height = self.size.height
-
-    def moved(self, offset):
-        return RectangleBBox(self.position.add(Vector.vectorize(offset)), self.size)
-
-    def scaled(self, scale):
-        return RectangleBBox(self.position, self.size + Size.make_size(scale))
-
-    def copy(self):
-        return RectangleBBox((self.x, self.y), (self.width, self.height))
-
-
-class Margin(object):
-    def __init__(self, top=0, right=0, bottom=0, left=0):
-        """
-        @type top: int
-        @type right: int
-        @type bottom: int
-        @type left: int
-        """
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.left = left
-
-
 class Drawable(object):
     def __init__(self):
         self.position = Vector(0, 0)
         self.children = []
+        self.click_handler = ClickHandler(self)
 
     def set_position(self, position):
         self.position = Vector.vectorize(position)
@@ -302,8 +257,8 @@ class SimpleVarDrawable(AbsValueDrawable):
         """
         super(SimpleVarDrawable, self).__init__(value)
 
-        self.name_box = BoxedLabelDrawable(self.get_name(), Margin(10, 10, 10, 10))
-        self.value_box = BoxedLabelDrawable(self.get_value(), Margin(10, 10, 10, 10))
+        self.name_box = BoxedLabelDrawable(self.get_name(), Margin.all(5.0))
+        self.value_box = BoxedLabelDrawable(self.get_value(), Margin.all(5.0))
     
     def get_bbox(self, canvas):
         self.name_box.set_position(self.position)
@@ -341,11 +296,16 @@ class StructDrawable(SimpleVarDrawable):
         @type val: variable.Variable
         """
         super(StructDrawable, self).__init__(val)
-        self.struct_label = BoxedLabelDrawable("{0} {1}".format(val.type.type_category.nice_name(), val.name), Margin(5, 5, 5, 5))
+        self.struct_label = BoxedLabelDrawable("{0} {1}".format(val.type.type_category.nice_name(), val.name),
+                                               Margin.all(6.0))
         self.children = []
 
     def add_child(self, child):
+        """
+        @type child: Drawable
+        """
         self.children.append(child)
+        self.click_handler.propagate_handler(child.click_handler)
 
     def get_children_left_offset(self):
         return 10
---
+++ gui/drawing/geometry.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from drawing.size import Size
+from drawing.vector import Vector
+
+
+class RectangleBBox(object):
+    @staticmethod
+    def contain(bboxes):
+        min_x = bboxes[0].x
+        max_x = bboxes[0].x + bboxes[0].width
+        min_y = bboxes[0].y
+        max_y = bboxes[0].y + bboxes[0].height
+
+        for bbox in bboxes[1:]:
+            min_x = min(min_x, bbox.x)
+            max_x = max(max_x, bbox.x + bbox.width)
+            min_y = min(min_y, bbox.y)
+            max_y = max(max_y, bbox.y + bbox.height)
+
+        return RectangleBBox((min_x, min_y), (max_x - min_x, max_y - min_y))
+
+    def __init__(self, position=(0, 0), size=(0, 0)):
+        self.position = Vector.vectorize(position)
+        self.size = Size.make_size(size)
+
+        self.x = self.position.x
+        self.y = self.position.y
+        self.width = self.size.width
+        self.height = self.size.height
+
+    def moved(self, offset):
+        return RectangleBBox(self.position.add(Vector.vectorize(offset)), self.size)
+
+    def scaled(self, scale):
+        return RectangleBBox(self.position, self.size + Size.make_size(scale))
+
+    def copy(self):
+        return RectangleBBox((self.x, self.y), (self.width, self.height))
+
+
+class Margin(object):
+    @staticmethod
+    def all(size):
+        """
+        Makes a margin with all sub-margins equal to the given value.
+        @type size: float
+        @rtype: Margin
+        """
+        return Margin(size, size, size, size)
+
+    def __init__(self, top=0.0, right=0.0, bottom=0.0, left=0.0):
+        """
+        @type top: float
+        @type right: float
+        @type bottom: float
+        @type left: float
+        """
+        self.top = top
+        self.right = right
+        self.bottom = bottom
+        self.left = left
