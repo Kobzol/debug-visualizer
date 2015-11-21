03325acb1cbba1ff1a0e11238d1365fe772827fe
Jakub Beránek <berykubik@gmail.com>
2015-11-21 21:48:18 +0100

    TEST: divided tests into two files

--- tests/test_mi_basic.py
+++ tests/test_mi_basic.py
@@ -13,28 +13,3 @@ def test_load_file(debugger):
     assert debugger.load_binary(os.path.join(TEST_SRC_DIR, "test_breakpoint_basic"))
     assert debugger.state.is_set(DebuggerState.BinaryLoaded)
     assert debugger.file_manager.get_main_source_file() == os.path.join(TEST_SRC_DIR, "test_breakpoint_basic.cpp")
-
-
-def test_bp_add(debugger):
-    src_file = "src/test_breakpoint_basic.cpp"
-    line = 5
-
-    debugger.load_binary(os.path.join(TEST_SRC_DIR, "test_breakpoint_basic"))
-    assert debugger.breakpoint_manager.add_breakpoint(src_file, line)
-    assert not debugger.breakpoint_manager.add_breakpoint(src_file, 100)
-    assert not debugger.breakpoint_manager.add_breakpoint("", line)
-
-    debugger.breakpoint_manager.add_breakpoint(src_file, 1)
-
-    assert len(debugger.breakpoint_manager.get_breakpoints()) == 2
-
-
-def test_bp_remove(debugger):
-    src_file = "src/test_breakpoint_basic.cpp"
-    line = 5
-
-    debugger.load_binary(os.path.join(TEST_SRC_DIR, "test_breakpoint_basic"))
-    debugger.breakpoint_manager.add_breakpoint(src_file, line)
-
-    assert debugger.breakpoint_manager.remove_breakpoint(src_file, line)
-    assert len(debugger.breakpoint_manager.get_breakpoints()) == 0
