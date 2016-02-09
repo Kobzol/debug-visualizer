0020737a0c986b95a2a3ed8c3f3afb525fab4d14
Jakub Beránek <berykubik@gmail.com>
2016-02-09 22:42:02 +0100

    ENH: removed lldb import and ptrcl dump settings

--- gui/initialize.py
+++ gui/initialize.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import prctl
-import resource
 
 import paths
-sys.path.append(paths.DIR_GUI)
+
 sys.path.append(paths.DIR_DEBUGGER)
-sys.path.append(paths.LLDB_LOCATION)
 
 
 from config import Config
@@ -15,9 +12,6 @@ from app import VisualiserApp
 
 
 if __name__ == '__main__':
-    prctl.set_dumpable(True)
-    resource.setrlimit(resource.RLIMIT_CORE, (resource.RLIM_INFINITY,resource.RLIM_INFINITY))
-
     Config.preload()
     app = VisualiserApp()
     app.start()
--- gui/paths.py
+++ gui/paths.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-import subprocess
 
 DIR_GUI = os.path.dirname(os.path.abspath(__file__))
 DIR_ROOT = os.path.dirname(DIR_GUI)
 
 DIR_DEBUGGER = os.path.join(DIR_ROOT, "debugger")
 DIR_RES = os.path.join(DIR_ROOT, "res")
-
-lldb_loc_proc = subprocess.Popen(["lldb", "-P"], stdout=subprocess.PIPE)
-LLDB_LOCATION = lldb_loc_proc.stdout.readline().strip()
 
 
 def root_path(path):
