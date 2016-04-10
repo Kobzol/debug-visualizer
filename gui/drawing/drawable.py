b35329bd2d75e844538fe2197e10e4a5a37e29fd
Jakub Beránek <berykubik@gmail.com>
2016-04-10 14:53:07 +0200

    REF: added GNU GPLv3 license headers to source files

--- gui/drawing/drawable.py
+++ gui/drawing/drawable.py
@@ -1,3 +1,22 @@
+#
+#    Copyright (C) 2015-2016 Jakub Beranek
+#
+#    This file is part of Devi.
+#
+#    Devi is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Devi is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Devi.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # -*- coding: utf-8 -*-
 
 import cairo
